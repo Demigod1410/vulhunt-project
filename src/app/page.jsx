ebe331c899ceae1b9b967ea277d5535192bd50ff
DemiGod <demigod14100@gmail.com>
2025-11-15 17:42:00 +0530

    Add spacing between import statements for improved readability

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
 import HeroSection from "@/components/herosection";
 import WhyUs from "@/components/whyus";
 import Services from "@/components/services";
+
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+
 
 export default function Home() {
   return (
